1f39b97708f0f8f2e419c465eaccd75dfd1e47f1
Vanessa Seva <vanessa.seva@fabrick.com>
2021-04-21 15:17:49 +0200

    flags-filter

---
+++ src/components/Card.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Card = (props) => {
+
+    const country = props.country;
+
+    const numberFormat = (x) => {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " "); // ts les 3 caracteres = mettre un espace vide
+
+    }
+
+    return (
+       <li className="card">
+           <img src={country.flag} alt="flag"/>
+           <div className="data-container">
+               <ul>
+                   <li>{country.name}</li>
+                   <li>{country.capital}</li>
+                   <li>Pop. {numberFormat(country.population)}</li>
+               </ul>
+           </div>
+       </li>
+    )
+}
+
+export default Card;
---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Navigation />
+            <Logo />
+            <h1>About</h1>
+            <br/>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus praesentium facilis esse earum officia unde impedit! Molestiae architecto adipisci debitis, sed rem iste impedit officia perspiciatis corporis placeat dignissimos natus accusamus blanditiis obcaecati saepe dicta exercitationem molestias error temporibus officiis, dolor, deserunt dolores! Sint ut quisquam eaque dicta doloremque. Temporibus.
+            </p>
+            <br/>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit dolorum perferendis illum delectus commodi! Totam praesentium ipsum deserunt iusto impedit rerum numquam quas dolores itaque, amet dolor eaque dolorum tempora qui facilis ipsam nostrum dicta sunt ratione fuga optio? Optio quo natus numquam excepturi veniam sapiente quod dolorum omnis ut nisi quas illum accusamus itaque, pariatur quis repudiandae, eaque neque dolore. Reprehenderit eveniet praesentium eligendi facilis soluta? Similique doloribus dolorem, sunt, aspernatur excepturi ex rem perspiciatis quidem ad nam exercitationem. Commodi error nemo exercitationem animi. Beatae eum alias laborum tempora?</p>
+        </div>
+    )
+}
+
+export default About;
